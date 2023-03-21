26647ec70d3b9896e03c46ee7f9cefb4083eed13
DmitriiBukhvalov <dimabukhvalov@gmail.com>
2023-03-21 23:45:56 +0300

    PITPM21
    PITPM21

--- main2.py
+++ main2.py
@@ -1,18 +1,26 @@
-from flask import Flask
+
+from flask import Flask, request, current_app
+from main import app
+
+
+with app.test_request_context('/products'):
+    request.path  
+    request.method
+    current_app.name
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello Flask'
-
-@app.route('/user//')
-def user_profile(user_id):
-    return "Profile page of user #{}".format(user_id)
-
-@app.route('/books//')
-def books(genre):
-    return "All Books in {} category".format(genre)
+def requestdata():
+    return "Hello! Your IP is {} and you are using {}: ".format(request.remote_addr,  
+                                                                request.user_agent)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT=int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT= 5555
+    app.run(HOST, PORT)
